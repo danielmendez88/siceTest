ae2e6eec32d274944fb510aaf689266e8a390e55
daniel mendez cruz <danielmendez.88@live.com>
2020-12-16 14:45:41 -0600

    modificaciones de vista y form cerss

--- resources/views/layouts/pages/sid_cerss_update.blade.php
+++ resources/views/layouts/pages/sid_cerss_update.blade.php
@@ -51,15 +51,15 @@
                 </div>
                 <!--NOMBRE CERSS END-->
                 <div class="form-group col-md-8">
-                    <label for="direcciones_cerss_update " class="control-label">DIRECCIÓN DEL CERSS</label>
-                    <input type="text" class="form-control" id="direcciones_cerss_update " name="direcciones_cerss_update " autocomplete="off" value="{{$alumnoPre_update->direccion_cerss}}"/>
+                    <label for="direcciones_cerss_update_ " class="control-label">DIRECCIÓN DEL CERSS</label>
+                    <input type="text" class="form-control" id="direcciones_cerss_update_ " name="direcciones_cerss_update_ " autocomplete="off" value="{{$alumnoPre_update->direccion_cerss}}"/>
                 </div>
             </div>
             <div class="form-row">
                 <!--TITULAR DEL CERSS-->
                 <div class="form-group col-md-8">
-                    <label for="titular_cerss_update " class="control-label">TITULAR DEL CERSS</label>
-                    <input type="text" class="form-control" id="titular_cerss_update " name="titular_cerss_update " autocomplete="off" value="{{$alumnoPre_update->titular_cerss}}"/>
+                    <label for="titular_cerss_update_ " class="control-label">TITULAR DEL CERSS</label>
+                    <input type="text" class="form-control" id="titular_cerss_update_ " name="titular_cerss_update_ " autocomplete="off" value="{{$alumnoPre_update->titular_cerss}}"/>
                 </div>
                 <!--TITULAR DEL CERSS END-->
             </div>
