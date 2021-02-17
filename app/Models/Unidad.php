62cfc88ae004f23f2a8d91cbf4809f947c62257f
Orlando Chávez <60703406+oorlandochavez@users.noreply.github.com>
2021-02-17 14:59:54 -0600

    07022021 02 addcp tbl_unidades

--- app/Models/Unidad.php
+++ app/Models/Unidad.php
@@ -12,7 +12,8 @@ class Unidad extends Model
 
     protected $fillable = [
         'id','unidad','cct','dunidad','dgeneral','plantel','academico','vinculacion','dacademico','pdgeneral',
-        'pdacademico', 'pdunidad', 'pacademico', 'pvinculacion', 'jcyc', 'pjcyc', 'ubicacion'
+        'pdacademico', 'pdunidad', 'pacademico', 'pvinculacion', 'jcyc', 'pjcyc', 'ubicacion','direccion',
+        'telefono','correo','coordenadas','codigo_postal'
     ];
 
     protected $hidden = ['created_at', 'updated_at'];
